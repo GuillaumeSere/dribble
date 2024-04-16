5da3a7e80b5442130bbe4d9d5382cfbb973b4375
Guillaume Sere <guillaumesere@hotmail.fr>
2024-04-16 12:54:30 +0200

    add speedInsights

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/HeroSection/Hero";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="">
         <Hero />
+        <SpeedInsights/>
     </main>
   );
 }
